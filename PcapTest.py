bb2c4a7644ef7e3d8bc7bad76bfe7ece29fe5736
JoyChenCN <354215459@qq.com>
2018-12-18 22:22:30 +0800

    fixed &

--- PcapTest.py
+++ PcapTest.py
@@ -25,7 +25,7 @@ class Sniffer:
             'Protocol': None,
             'Source Address': None,
             'Destination Address': None
-        
+
         }
         self.Packet_UDP = {
             'Source_port': None,
@@ -49,8 +49,8 @@ class Sniffer:
             'Packet_ID': None,
             'Sequence': None,
             'Data_seg': None
-}
-    
+        }
+
     def change_digit_to_word(self, protocol):
         protocols = {
             '0': 'IP',
@@ -59,41 +59,41 @@ class Sniffer:
             '17': 'UDP'
         }
         return protocols[str(protocol)]
-    
+
     def soc_establish_conn(self, param):
-        
+
         '''
-            To create a socket
-            
-            :param param: choose a param to parse the packet with protocol
-            :return: nil
-            '''
-        
+        To create a socket
+
+        :param param: choose a param to parse the packet with protocol
+        :return: nil
         '''
-            (You can skip it)
-            I just want to say something about the `socket.AF_INET` & `socket.AF_PACKET`.
-            When I use this word first,I only can get one protocol just like `TCP`, `UDP`, or `ICMP`...
-            So, should I neet to use Multiprocessing Pool ??
-            I have been thinking for a long time about multi-process parallelism ...
-            But, When I saw the anotation about `AF_PACKET` I get a clear idea. Why not unpack the MAC-Packet?
-            Ohhhh!
-            
-            You can see the word about AF_PACKET =>  `When using socket.AF_PACKET to create a socket,
-            it will be able to capture all Ethernet frames received or sent by the unit.`
-            
-            So, the final version change to use AF_PACKET, I don't need to care about multi-process!
-            
-            '''
+
+        '''
+        (You can skip it)
+        I just want to say something about the `socket.AF_INET` & `socket.AF_PACKET`.  
+        When I use this word first,I only can get one protocol just like `TCP`, `UDP`, or `ICMP`...
+        So, should I need to use Multiprocessing Pool ??
+        I have been thinking for a long time about multi-process parallelism ...
+        But, When I saw the annotation about `AF_PACKET` I get a clear idea. Why not unpack the MAC-Packet?
+        Ohhhh!
+
+        You can see the word about AF_PACKET =>  `When using socket.AF_PACKET to create a socket, 
+        it will be able to capture all Ethernet frames received or sent by the unit.`
+
+        So, the final version change to use AF_PACKET, I don't need to care about multi-process!
+
+        '''
         self.param = param
         try:
             self.s = socket.socket(socket.AF_INET, socket.SOCK_RAW, self.param)
-        # self.s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+            # self.s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
         except:
             print(str(self.param), '# Socket could not be created')
             exit(-1)
         print(str(self.param), '# Socket established success!')
         self.unpack_ip_packet()
-    
+
     def unpack_ip_packet(self):
         while True:
             packet = self.s.recvfrom(65565)
@@ -125,15 +125,15 @@ class Sniffer:
             elif protocol == 1:
                 self.unpack_icmp_packet(iph_lenth, packet)
 
-def unpack_tcp_packet(self, iph_lenth, packet):
-    tcp_header = packet[iph_lenth:iph_lenth + 20]
-    tcph = unpack('!HHLLBBHHH', tcp_header)
-    source_port = tcph[0]
-    dest_port = tcph[1]
-    sequence = tcph[2]
-    acknowledgement = tcph[3]
-    doff_reserved = tcph[4]
-    tcph_length = doff_reserved >> 4
+    def unpack_tcp_packet(self, iph_lenth, packet):
+        tcp_header = packet[iph_lenth:iph_lenth + 20]
+        tcph = unpack('!HHLLBBHHH', tcp_header)
+        source_port = tcph[0]
+        dest_port = tcph[1]
+        sequence = tcph[2]
+        acknowledgement = tcph[3]
+        doff_reserved = tcph[4]
+        tcph_length = doff_reserved >> 4
         h_size = iph_lenth + tcph_length + 4
         data_size = len(packet) - h_size
         # get data from the packet
@@ -146,26 +146,26 @@ def unpack_tcp_packet(self, iph_lenth, packet):
         self.Packet_TCP['Data_seg'] = data
         for key, value in self.Packet_TCP.items():
             print(key, ':', value, end=' | ')
-    print()
+        print()
         print()
 
-def unpack_udp_packet(self, iph_lenth, packet):
-    udp_header = packet[iph_lenth:iph_lenth + 8]
-    udph = unpack('!HHHH', udp_header)
-    source_port = udph[0]
-    dest_port = udph[1]
-    length = udph[2]
-    checksum = udph[3]
-    data = packet[28:len(packet)]
-    self.Packet_UDP['Source_port'] = source_port
-    self.Packet_UDP['Dest_port'] = dest_port
-    self.Packet_UDP['Length'] = length
-    self.Packet_UDP['Checksum'] = checksum
-    self.Packet_UDP['Data_seg'] = data
-    for key, value in self.Packet_UDP.items():
-        print(key, ':', value, end=' | ')
+    def unpack_udp_packet(self, iph_lenth, packet):
+        udp_header = packet[iph_lenth:iph_lenth + 8]
+        udph = unpack('!HHHH', udp_header)
+        source_port = udph[0]
+        dest_port = udph[1]
+        length = udph[2]
+        checksum = udph[3]
+        data = packet[28:len(packet)]
+        self.Packet_UDP['Source_port'] = source_port
+        self.Packet_UDP['Dest_port'] = dest_port
+        self.Packet_UDP['Length'] = length
+        self.Packet_UDP['Checksum'] = checksum
+        self.Packet_UDP['Data_seg'] = data
+        for key, value in self.Packet_UDP.items():
+            print(key, ':', value, end=' | ')
         print()
-    
+
     def unpack_icmp_packet(self, iph_lenth, packet):
         icmp_header = packet[iph_lenth:iph_lenth + 8]
         data = packet[28:len(packet)]
@@ -193,5 +193,5 @@ if __name__ == '__main__':
         # pool.map(snif.soc_establish_conn, params)   # udp will cause suspended
         snif.soc_establish_conn(params[2])
     except:
-        print('*'*20)
+        print('*' * 20)
         print('HALTED!')
